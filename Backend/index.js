05dc2caec591ae2e9d79be3ff26c5a2fd145d936
rg10rahul <rgrahul149@gmail.com>
2025-09-01 12:19:57 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -9,8 +9,8 @@ dotenv.config();
 const Port = process.env.Port;
 const Mongodburl = process.env.Mongodburl;
 app.use(express.json());
-app.get("/", aysnc() {
-  console.log("Backend is running")
+app.get("/", aysnc(req,res) {
+  res.send("Backend is running")
 })
 try {
   Mongodbconnect(Mongodburl);
